46329e3c89b730d329cc1228b33d3be632136e4f
jpark <JamesYPark90@gmail.com>
2018-03-26 22:20:00 -0700

    more cleanup

--- gdax.py
+++ gdax.py
@@ -5,16 +5,16 @@ import serial
 import dateutil.parser
 
 if len(sys.argv) < 2:
-    print("Usage: {} pair".format(sys.argv[0]))
-    exit()
+    print("Usage: {} <pair>".format(sys.argv[0]))
+    exit(1)
 
-#sample_pair = 'ETH-USD'
+# sample_pair = 'ETH-USD'
 pair = sys.argv[1]
 endpointURL = 'https://api.gdax.com/products/{}/ticker'.format(pair)
 
-ser = serial.Serial('/dev/ttyACM0',9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
-#wait for the arduino to respond
+# wait for the arduino to respond
 print(ser.readline())
 
 while True:
@@ -22,11 +22,11 @@ while True:
     ticker = page.json()
 
     price = float(ticker['price'])
-    localDateTime= dateutil.parser.parse(ticker['time']).astimezone()
-    date = localDateTime.strftime("%x")
-    localTime = localDateTime.strftime("%I:%M:%S %p")
+    dateTime = dateutil.parser.parse(ticker['time']).astimezone()
+    date = dateTime.strftime("%x")
+    localTime = dateTime.strftime("%I:%M:%S %p")
 
     print("{},{:.2f},{},{}".format(pair, price, localTime, date))
-    ser.write(bytes("{},{:.2f},{},{}\x00".format(pair,price,localTime,date), 'utf-8'))
+    ser.write(bytes("{},{:.2f},{},{}\x00".format(pair, price, localTime, date), 'utf-8'))
 
     time.sleep(5)
